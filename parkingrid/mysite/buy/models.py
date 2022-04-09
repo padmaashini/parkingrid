0c0d716c41ecaa9c3fd6dfe7874dedbf79d9f61c
Padmaashini Sukumaran <padmaashini@medmehealth.com>
2022-04-09 13:50:00 -0400

    created model for tickets

--- parkingrid/mysite/buy/models.py
+++ parkingrid/mysite/buy/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Tickets(models.Model):
+    DURATIONS = [
+        (30, '30 minutes'), 
+        (60, '1 hour'), 
+        (120, '2 hours')
+    ]
+    time_issued = models.DateField()
+    license_plate = models.CharField(max_length=10)
+    duration = models.IntegerField(choices = DURATIONS)
+
+    def __str__(self):
+        return self.license_plate
